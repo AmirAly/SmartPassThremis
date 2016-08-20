b41c632978edc00d751e638948f1771b53cf8b68
AmirAly <amiraly@deltacode.co>
2016-08-20 21:25:06 +0200

    QR Format

--- SmartPass/ScanCodeController.cs
+++ SmartPass/ScanCodeController.cs
@@ -48,6 +48,14 @@ namespace SmartPass
 				}
 				else
 				{
+					//string _QRCode = "{'jwt':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg'}";
+					//string Signature = "11CBBF516FA74976D1AAFF6FD295EF60A7C030405DF7DDB276A2E6E17C4E481845CD9861DFCE17ED64D139272C462D0EBEA3583782E7F589E31AC3E683B9E9E466BA186ACA 1F 2B C1 6C 9A 6D 3E CB 0C F5 5F FB 8E 38 94 08 16 6C 8D E3 28 1B C6 61 FA 16 DD E3 B3 B5 36 63 03 A3 28 B6 92 18 AE 27 0D 9A 64 30 82 03 C2 87 C5 EB 7E 8D AD 4F 60 E8 74 DF 89 06 C5 04 47 AB CC F7 F1 B3 38 FC FE 25 48 AE 90 A8 1E 0D ED 3D 1B 22 F4 98 84 7F 4E 45 B2 AE 15 3C 33 A1 28 3E 20 76 27 7F A9 95 0F B7 EC 66 79 55 9F AB 2C 9F 42 B3 DE E6 C2 88 ED 2D D4 AC 03 0D 34 D1 91 53 EB 38 84 77 30 0E F0 B2 F5 50 D5 30 60 AE E2 C4 FB F1 60 09 1B 5A 45 17 D0 A0 C9 06 3D 9E 11 23 C4 3E 8E E2 EE 2A AB A9 F4 A2 7E 0A 4D 48 EC 42 47 31 6D 94 34 B7 4C 65 F7 FE D9";
+					//Signature = System.Text.RegularExpressions.Regex.Replace(Signature, @"\s+", "");
+					//JObject jObject_Encoded = JObject.Parse(_QRCode);
+					//string JWT_Encoded_String = jObject_Encoded["jwt"].ToString();
+					//string JWT_String_Decoded = JsonWebToken.Decode(JWT_Encoded_String, "", false);
+					//JObject JWT_Decoded = JObject.Parse(JWT_String_Decoded);
+					//tbCode.Text = JWT_Decoded["aud"].ToString();
 					generateCodeAndNavigate(tbCode.Text);
 
 				}
@@ -80,14 +88,16 @@ namespace SmartPass
 				result = await scanner.Scan();
 				if (result != null)
 				{
-					tbCode.Text = result.Text;
-					string _QRCode = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg";
-					//string Signature = "11CBBF516FA74976D1AAFF6FD295EF60A7C030405DF7DDB276A2E6E17C4E481845CD9861DFCE17ED64D139272C462D0EBEA3583782E7F589E31AC3E683B9E9E466BA186ACA 1F 2B C1 6C 9A 6D 3E CB 0C F5 5F FB 8E 38 94 08 16 6C 8D E3 28 1B C6 61 FA 16 DD E3 B3 B5 36 63 03 A3 28 B6 92 18 AE 27 0D 9A 64 30 82 03 C2 87 C5 EB 7E 8D AD 4F 60 E8 74 DF 89 06 C5 04 47 AB CC F7 F1 B3 38 FC FE 25 48 AE 90 A8 1E 0D ED 3D 1B 22 F4 98 84 7F 4E 45 B2 AE 15 3C 33 A1 28 3E 20 76 27 7F A9 95 0F B7 EC 66 79 55 9F AB 2C 9F 42 B3 DE E6 C2 88 ED 2D D4 AC 03 0D 34 D1 91 53 EB 38 84 77 30 0E F0 B2 F5 50 D5 30 60 AE E2 C4 FB F1 60 09 1B 5A 45 17 D0 A0 C9 06 3D 9E 11 23 C4 3E 8E E2 EE 2A AB A9 F4 A2 7E 0A 4D 48 EC 42 47 31 6D 94 34 B7 4C 65 F7 FE D9";
-					//Signature = System.Text.RegularExpressions.Regex.Replace(Signature, @"\s+", "");
-					string JWT = JsonWebToken.Decode(_QRCode, "", false);
-					JObject jObject = JObject.Parse(JWT);
-					tbCode.Text = jObject["aud"].ToString();
-					generateCodeAndNavigate(tbCode.Text);
+				string _QRCode = "{'jwt':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg'}";
+				//string Signature = "11CBBF516FA74976D1AAFF6FD295EF60A7C030405DF7DDB276A2E6E17C4E481845CD9861DFCE17ED64D139272C462D0EBEA3583782E7F589E31AC3E683B9E9E466BA186ACA 1F 2B C1 6C 9A 6D 3E CB 0C F5 5F FB 8E 38 94 08 16 6C 8D E3 28 1B C6 61 FA 16 DD E3 B3 B5 36 63 03 A3 28 B6 92 18 AE 27 0D 9A 64 30 82 03 C2 87 C5 EB 7E 8D AD 4F 60 E8 74 DF 89 06 C5 04 47 AB CC F7 F1 B3 38 FC FE 25 48 AE 90 A8 1E 0D ED 3D 1B 22 F4 98 84 7F 4E 45 B2 AE 15 3C 33 A1 28 3E 20 76 27 7F A9 95 0F B7 EC 66 79 55 9F AB 2C 9F 42 B3 DE E6 C2 88 ED 2D D4 AC 03 0D 34 D1 91 53 EB 38 84 77 30 0E F0 B2 F5 50 D5 30 60 AE E2 C4 FB F1 60 09 1B 5A 45 17 D0 A0 C9 06 3D 9E 11 23 C4 3E 8E E2 EE 2A AB A9 F4 A2 7E 0A 4D 48 EC 42 47 31 6D 94 34 B7 4C 65 F7 FE D9";
+				//Signature = System.Text.RegularExpressions.Regex.Replace(Signature, @"\s+", "")
+				_QRCode = result.Text;
+				JObject jObject_Encoded = JObject.Parse(_QRCode);
+				string JWT_Encoded_String = jObject_Encoded["jwt"].ToString();
+				string JWT_String_Decoded = JsonWebToken.Decode(JWT_Encoded_String, "", false);
+				JObject JWT_Decoded = JObject.Parse(JWT_String_Decoded);
+				tbCode.Text = JWT_Decoded["aud"].ToString();
+				generateCodeAndNavigate(tbCode.Text);
 					btnPeer.Enabled = false;
 					scanner = null;
 					result = null;
