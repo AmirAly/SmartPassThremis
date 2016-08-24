901fac0a537e572a10c611e52437003d8e8c8610
AmirAly <amiraly@deltacode.co>
2016-08-24 22:26:47 +0200

    Mods

--- SmartPass/ScanCodeController.cs
+++ SmartPass/ScanCodeController.cs
@@ -59,7 +59,7 @@ namespace SmartPass
 				scanner.BottomText = "Please align the code in the middle of the view";
 
 			}
-				//result = await scanner.Scan();
+				result = await scanner.Scan();
 				if (result != null)
 				{
 				string _QRCode = "{\r\n  \"jwt\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg\"\r\n}";
@@ -95,9 +95,9 @@ namespace SmartPass
 					result = null;
 					GC.Collect();
 				}
-				catch (Exception ex)
+				catch
 				{
-					var alert = new UIAlertView("Error", "Unexpected code scanned: " +_QRCode, null, "Ok", null);
+					var alert = new UIAlertView("Error", "Unexpected code scanned: ", null, "Ok", null);
 					alert.Show();
 				}
 				}
