1beb7b9bccbdc3d310a405a4b9e8e42353905d43
AmirAly <amiraly@deltacode.co>
2016-09-05 22:58:20 +0200

    m

--- SmartPass/CustomScanner.cs
+++ SmartPass/CustomScanner.cs
@@ -20,7 +20,6 @@ namespace SmartPass
 	public class CustomOverlayView : ZXing.Mobile.ZXingScannerView
 	{
 		public UIButton ButtonCancel;
-		public UIButton ScanBorder;
 		nfloat TopMargin = 200;
 		nfloat SideMargins = 50;
 		public CustomOverlayView()
@@ -32,15 +31,6 @@ namespace SmartPass
 			ButtonCancel.AutoresizingMask = UIViewAutoresizing.FlexibleTopMargin | UIViewAutoresizing.FlexibleRightMargin;
 			ButtonCancel.SetTitle("Cancel", UIControlState.Normal);
 			this.Subviews[0].AddSubview(ButtonCancel);
-			ButtonCancel.TouchUpInside += (sender, e) =>
-			{
-				
-			};
-			ScanBorder = UIButton.FromType(UIButtonType.RoundedRect);
-			ScanBorder.Frame = new CGRect(0, TopMargin, this.Frame.Width / 2 - 100, 100);
-			ScanBorder.AutoresizingMask = UIViewAutoresizing.FlexibleTopMargin | UIViewAutoresizing.FlexibleRightMargin;
-			ScanBorder.SetTitle("", UIControlState.Normal);
-			this.Subviews[0].AddSubview(ScanBorder);
 		}
 		public void HandleScan(ZXing.Result _res)
 		{
@@ -48,75 +38,7 @@ namespace SmartPass
 		}
 		public override void Draw(CGRect rect)
 		{
-			TopMargin = this.Frame.Height / 3;
-			SideMargins = this.Frame.Width / 6;
- 			using (CGContext g = UIGraphics.GetCurrentContext())
-			{
-							//set up drawing attributes
-				g.SetLineWidth(0);
-				UIColor.DarkGray.SetFill();
-				UIColor.Black.SetStroke();
-
-				//Create top Rect
-				var path = new CGPath();
-
-				path.AddLines(new CGPoint[]{
-					new CGPoint (0, 0),
-					new CGPoint (this.Frame.Width, 0),
-					new CGPoint (this.Frame.Width, TopMargin),
-					new CGPoint (0, TopMargin)});
-
-				path.CloseSubpath();
-
-				g.AddPath(path);
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-
-				//Create bottom Rect
-				path = new CGPath();
-
-				path.AddLines(new CGPoint[]{
-					new CGPoint (0, this.Frame.Height),
-					new CGPoint (this.Frame.Width, this.Frame.Height),
-					new CGPoint (this.Frame.Width, this.Frame.Height-TopMargin),
-					new CGPoint (0, this.Frame.Height-TopMargin)});
-
-				path.CloseSubpath();
-
-				g.AddPath(path);
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-
-
-				//Create left rect
-				path = new CGPath();
-
-				path.AddLines(new CGPoint[]{
-					new CGPoint (0, TopMargin),
-					new CGPoint (SideMargins, TopMargin),
-					new CGPoint (SideMargins, this.Frame.Height-TopMargin),
-					new CGPoint (0, this.Frame.Height-TopMargin)});
-
-				path.CloseSubpath();
-
-				//add geometry to graphics context and draw it
-				g.AddPath(path);
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-
-				//Create right rect
-				path = new CGPath();
-
-				path.AddLines(new CGPoint[]{
-					new CGPoint (this.Frame.Width-SideMargins, TopMargin),
-					new CGPoint (this.Frame.Width, TopMargin),
-					new CGPoint (this.Frame.Width, this.Frame.Height-TopMargin),
-					new CGPoint (this.Frame.Width-SideMargins, this.Frame.Height-TopMargin)});
-
-				path.CloseSubpath();
-
-				//add geometry to graphics context and draw it
-				g.AddPath(path);
-				g.DrawPath(CGPathDrawingMode.FillStroke);
-			}
-
+			base.Draw(rect);
 		}
 		public override void LayoutSubviews()
 		{
@@ -124,17 +46,6 @@ namespace SmartPass
 			ButtonCancel.SetTitleColor(UIColor.White, UIControlState.Normal);
 			ButtonCancel.Layer.BorderWidth = 1;
 			ButtonCancel.Layer.BorderColor = new CGColor(255, 255, 255);
-			this.Subviews[1].Hidden = true;
-			this.Subviews[0].Layer.ZPosition = 2;
-
-			//ScanBorder
-			ScanBorder.Frame = new CGRect(SideMargins, TopMargin , this.Frame.Width - (2*SideMargins), TopMargin+ (TopMargin / 2));
-			ScanBorder.SetTitleColor(UIColor.White, UIControlState.Normal);
-			ScanBorder.Layer.ZPosition = 1;
-			ScanBorder.SetTitleColor(UIColor.White, UIControlState.Normal);
-			ScanBorder.Layer.BorderWidth = 1;
-			ScanBorder.Layer.BorderColor = new CGColor(255, 255, 255);
-			ScanBorder.Layer.ZPosition = 1;
 		}
 
 	}
