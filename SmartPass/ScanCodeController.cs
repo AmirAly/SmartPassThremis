b6ce469dbd2895d6d1acbcce3b639877c01dbb01
AmirAly <amiraly@deltacode.co>
2016-09-07 12:04:42 +0200

    Scanner Square

--- SmartPass/ScanCodeController.cs
+++ SmartPass/ScanCodeController.cs
@@ -22,40 +22,60 @@ namespace SmartPass
 	{
 		AVCaptureVideoPreviewLayer layer;
 		NSUserDefaults user = NSUserDefaults.StandardUserDefaults;
-
+		float TopMargin;
+		float SideMargins;
+		UILabel label;
+		UILabel lblSq;
 		public ContentView(UIColor fillColor, AVCaptureVideoPreviewLayer layer, MyMetadataOutputDelegate metadataSource)
 		{
 			BackgroundColor = fillColor;
 
-			float TopMargin = (float)this.Frame.Height / 3;
-			float SideMargin = (float)this.Frame.Width / 6;
+			 TopMargin = (float)this.Frame.Height / 3;
+			 SideMargins = (float)this.Frame.Width / 6;
 			this.layer = layer;
 			layer.MasksToBounds = true;
-			layer.VideoGravity = AVLayerVideoGravity.ResizeAspectFill;
+			layer.VideoGravity = AVLayerVideoGravity.ResizeAspect;
 
 			Frame = UIScreen.MainScreen.Bounds;
 			layer.Frame = Frame;
 			Layer.AddSublayer(layer);
 
-			var label = new UILabel(new RectangleF(TopMargin-60, SideMargin,(float)(this.Frame.Width - (SideMargin*2)),50));
+			label = new UILabel(new RectangleF(TopMargin-60, SideMargins,(float)(this.Frame.Width - (SideMargins*2)),50));
 			label.Text = "Please center the code in the middle of te square";
+			label.BackgroundColor = UIColor.Black;
 			AddSubview(label);
-			var lblSq = new UILabel(new RectangleF(TopMargin, SideMargin, (float)(this.Frame.Width - (SideMargin * 2)), TopMargin));
-			lblSq.Text = "";
+			lblSq = new UILabel(new RectangleF(TopMargin, SideMargins, (float)(this.Frame.Width - (SideMargins * 2)), TopMargin));
+			lblSq.Text = "_________________________";
 			lblSq.Layer.BorderColor = new CGColor(255,255,255);
 			lblSq.Layer.BorderWidth = 2;
 			AddSubview(lblSq);
-
+			bool _Scanned = false;
 			metadataSource.MetadataFound += (s, e) => {
+				if (_Scanned)
+					return;
+				var alert = new UIAlertView("Info", "Captured", null, "Ok", null);
+				alert.Show();
+				_Scanned = true;
 				processQRCode(e.StringValue);
 			};
 
 		}
-
 		public override void LayoutSubviews()
 		{
-			base.LayoutSubviews();
+
 			layer.Frame = Bounds;
+			TopMargin = (float)this.Frame.Height / 3;
+			SideMargins = (float)this.Frame.Width / 6;
+
+			label.Frame = new CGRect(0,TopMargin-70, SideMargins*6, 60);
+			label.TextColor = UIColor.Black;
+			label.Text = "Please center the code in the middle of te square";
+			lblSq.Frame = new CGRect(SideMargins, TopMargin, (float)(this.Frame.Width - (SideMargins * 2)), TopMargin);
+			lblSq.Text = "";
+			label.BackgroundColor = UIColor.White;
+			label.TextAlignment = UITextAlignment.Center;
+			base.LayoutSubviews();
+
 		}
 		public void generateCodeAndNavigate(string _key)
 		{
@@ -80,6 +100,11 @@ namespace SmartPass
 					string _QRCode = "{\r\n  \"jwt\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg\"\r\n}";
 					_QRCode = _result.Replace("\\r\\n \\", "");
 					_QRCode = _QRCode.Replace("\\r\\n", "");
+					_QRCode = _QRCode.Replace("\\", "");
+					_QRCode = _QRCode.Replace("\\", "");
+					_QRCode = _QRCode.Replace("\\", "");
+					_QRCode = _QRCode.Replace("\\", "");
+
 					JObject jObject_Encoded = JObject.Parse(_QRCode);
 					string JWT_Encoded_String = jObject_Encoded["jwt"].ToString();
 					string JWT_String_Decoded = JsonWebToken.Decode(JWT_Encoded_String, "", false);
@@ -148,14 +173,11 @@ namespace SmartPass
 				metadataOutput.SetDelegate(metadataDelegate, DispatchQueue.MainQueue);
 				session.AddOutput(metadataOutput);
 				//Confusing! *After* adding to session, tell output what to recognize...
-
 				metadataOutput.MetadataObjectTypes = AVMetadataObjectType.QRCode;
 
 				var previewLayer = new AVCaptureVideoPreviewLayer(session);
 				var view = new ContentView(UIColor.Blue, previewLayer, metadataDelegate);
-
 				session.StartRunning();
-
 				this.View = view;
 
 			};
