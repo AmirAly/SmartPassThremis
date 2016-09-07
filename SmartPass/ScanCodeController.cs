74b2cef7aeb2612c6f049706af16d2a29420c836
AmirAly <amiraly@deltacode.co>
2016-09-07 03:05:17 +0200

    MM

--- SmartPass/ScanCodeController.cs
+++ SmartPass/ScanCodeController.cs
@@ -21,12 +21,14 @@ namespace SmartPass
 	public class ContentView : UIView
 	{
 		AVCaptureVideoPreviewLayer layer;
+		NSUserDefaults user = NSUserDefaults.StandardUserDefaults;
 
 		public ContentView(UIColor fillColor, AVCaptureVideoPreviewLayer layer, MyMetadataOutputDelegate metadataSource)
 		{
 			BackgroundColor = fillColor;
 
-
+			float TopMargin = (float)this.Frame.Height / 3;
+			float SideMargin = (float)this.Frame.Width / 6;
 			this.layer = layer;
 			layer.MasksToBounds = true;
 			layer.VideoGravity = AVLayerVideoGravity.ResizeAspectFill;
@@ -35,11 +37,16 @@ namespace SmartPass
 			layer.Frame = Frame;
 			Layer.AddSublayer(layer);
 
-			var label = new UILabel(new RectangleF(40, 80, 100, 80));
-			label.Text = "Scanning ...";
+			var label = new UILabel(new RectangleF(TopMargin-60, SideMargin,(float)(this.Frame.Width - (SideMargin*2)),50));
+			label.Text = "Please center the code in the middle of te square";
 			AddSubview(label);
+			var lblSq = new UILabel(new RectangleF(TopMargin, SideMargin, (float)(this.Frame.Width - (SideMargin * 2)), TopMargin));
+			lblSq.Text = "";
+			lblSq.Layer.BorderColor = new CGColor(255,255,255);
+			lblSq.Layer.BorderWidth = 2;
+			AddSubview(lblSq);
 
-			metadataSource.MetadataFound += (s, e) => label.Text = e.StringValue;
+			metadataSource.MetadataFound += (s, e) => { };
 
 		}
 
@@ -47,6 +54,53 @@ namespace SmartPass
 		{
 			base.LayoutSubviews();
 			layer.Frame = Bounds;
+		}
+		public void generateCodeAndNavigate(string _key)
+		{
+			user.SetString("PEER_" + _key, "PEER");
+			var content = this.InputViewController.Storyboard.InstantiateViewController("acController") as AccessCodeViewController;
+			var menu = this.InputViewController.Storyboard.InstantiateViewController("sbController") as SideBarViewController;
+			RootController.SidebarController = new SidebarController(this.InputViewController, content, menu);
+		}
+		public static byte[] StringToByteArray(string hex)
+		{
+			return Enumerable.Range(0, hex.Length)
+							 .Where(x => x % 2 == 0)
+							 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+							 .ToArray();
+		}
+		public void processQRCode(string _result)
+		{
+			try
+			{
+				if (_result != null)
+				{
+					string _QRCode = "{\r\n  \"jwt\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg\"\r\n}";
+					_QRCode = _result.Replace("\\r\\n \\", "");
+					_QRCode = _QRCode.Replace("\\r\\n", "");
+					JObject jObject_Encoded = JObject.Parse(_QRCode);
+					string JWT_Encoded_String = jObject_Encoded["jwt"].ToString();
+					string JWT_String_Decoded = JsonWebToken.Decode(JWT_Encoded_String, "", false);
+					JObject JWT_Decoded = JObject.Parse(JWT_String_Decoded);
+					long _ticks = long.Parse(JWT_Decoded["exp"].ToString());
+					DateTime _exp = new DateTime(_ticks);
+					if (_exp >= DateTime.Now)
+					{
+						var alert = new UIAlertView("Error", "This token is expired", null, "Ok", null);
+						alert.Show();
+						return;
+					}
+					var _aud = JWT_Decoded["aud"].ToString();
+					user.SetString(JWT_Decoded["iss"].ToString(), "PORTAL");
+					generateCodeAndNavigate(_aud);
+					GC.Collect();
+				}
+			}
+			catch (Exception ex)
+			{
+				var alert = new UIAlertView("Error", ex.Message, null, "Ok", null);
+				alert.Show();
+			}
 		}
 	}
 	public class MyMetadataOutputDelegate : AVCaptureMetadataOutputObjectsDelegate
@@ -73,9 +127,6 @@ namespace SmartPass
 			// Releases the view if it doesn't have a superview.
 			base.DidReceiveMemoryWarning();
 		}
-		public static ZXing.Mobile.MobileBarcodeScanner scanner;
-		public static ZXing.Result result;
-		NSUserDefaults user = NSUserDefaults.StandardUserDefaults;
 		public ScanCodeController (IntPtr handle) : base (handle)
         {
         }
@@ -84,98 +135,35 @@ namespace SmartPass
 			base.ViewDidLoad();
 			btnScan.TouchUpInside += (sender, e) =>
 			{
-				StartScanner();
+				session = new AVCaptureSession();
+				var camera = AVCaptureDevice.DefaultDeviceWithMediaType(AVMediaType.Video);
+				var input = AVCaptureDeviceInput.FromDevice(camera);
+				session.AddInput(input);
+
+				//Add the metadata output channel
+				metadataOutput = new AVCaptureMetadataOutput();
+				var metadataDelegate = new MyMetadataOutputDelegate();
+				metadataOutput.SetDelegate(metadataDelegate, DispatchQueue.MainQueue);
+				session.AddOutput(metadataOutput);
+				//Confusing! *After* adding to session, tell output what to recognize...
+
+				metadataOutput.MetadataObjectTypes = AVMetadataObjectType.QRCode;
+
+				var previewLayer = new AVCaptureVideoPreviewLayer(session);
+				var view = new ContentView(UIColor.Blue, previewLayer, metadataDelegate);
+
+				session.StartRunning();
+
+				this.View = view;
+
 			};
 			this.tbCode.ShouldReturn += (textField) =>
 			{
 				textField.ResignFirstResponder();
 				return true;
 			};
-			session = new AVCaptureSession();
-			var camera = AVCaptureDevice.DefaultDeviceWithMediaType(AVMediaType.Video);
-			var input = AVCaptureDeviceInput.FromDevice(camera);
-			session.AddInput(input);
-
-			//Add the metadata output channel
-			metadataOutput = new AVCaptureMetadataOutput();
-			var metadataDelegate = new MyMetadataOutputDelegate();
-			metadataOutput.SetDelegate(metadataDelegate, DispatchQueue.MainQueue);
-			session.AddOutput(metadataOutput);
-			//Confusing! *After* adding to session, tell output what to recognize...
-
-			metadataOutput.MetadataObjectTypes = AVMetadataObjectType.QRCode;
-
-			var previewLayer = new AVCaptureVideoPreviewLayer(session);
-			var view = new ContentView(UIColor.Blue, previewLayer, metadataDelegate);
-
-			session.StartRunning();
-
-			this.View = view;
 
 		}
-		public void generateCodeAndNavigate(string _key)
-		{
-			user.SetString("PEER_"+_key, "PEER");
-			var content = this.Storyboard.InstantiateViewController("acController") as AccessCodeViewController;
-			var menu = this.Storyboard.InstantiateViewController("sbController") as SideBarViewController;
-			RootController.SidebarController = new SidebarController(this, content, menu);
-		}
-		public static byte[] StringToByteArray(string hex)
-		{
-			return Enumerable.Range(0, hex.Length)
-							 .Where(x => x % 2 == 0)
-							 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-							 .ToArray();
-		}
-		public async void StartScanner()
-		{
-			scanner = new ZXing.Mobile.MobileBarcodeScanner();
-			scanner.UseCustomOverlay = false;
-			result = await scanner.Scan();
-		}
-		public  void StartScanner_(ZXing.Result result)
-		{
-			try
-			{
-				if (result != null)
-				{
-					string _QRCode = "{\r\n  \"jwt\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJJUCI6IjE5Mi4xNjguMS40IiwiVXNlciI6ImhkMUBpdGhlbWlzLnVrIiwiUm9sZXMiOiJ0aGVtaXNwYXNzIiwiZ3VpZCI6IjNjOTM1MWMwLTBjMzAtNDczNy1iZThlLTBjMTU5ZGRhMWI3YyIsIklkX2d1aWQiOiIwIiwiaXNzIjoic3ZjLm15bG9naW4uaW8iLCJhdWQiOiJubjJTNDVrNEdkIiwiZXhwIjoxNDcxNDI3MjgxLCJuYmYiOjE0NzE0MjYwODF9.QVj_fslCBZyY4oeNICyaFkgNsaoqxCwg-rXsR4oD0keNAtXywyNxyAVc5AQJKnKVDh3yXq36TzVdrS2RaU7gdx8kzO1nn5otWtg-xk7I1Wq3C_dELJh4jKdv215_s3jfSaDS1nWMoa0gmuxy9_T-42kIOinTlr58UFRZVgE2-I0o9ZxrSkrKcsVqqqptkBplsplSOua5u4UQ7N6a5ApSQZae1A8Jy9Xwz_yVLDEwI-ZM_Hmjn5WvR5DD27Eai2P7pyer9tVAlMkW3qlvUFsECQMMdAHqwolMhbPqO0fBfVSueNSBVSUTtVY1csd30nFJiTW0zL_TRjtz9_74cTmmZg\"\r\n}";
-					_QRCode = result.Text.Replace("\\r\\n \\", "");
-					_QRCode = _QRCode.Replace("\\r\\n", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					_QRCode = _QRCode.Replace("\\", "");
-					JObject jObject_Encoded = JObject.Parse(_QRCode);
-					string JWT_Encoded_String = jObject_Encoded["jwt"].ToString();
-					string JWT_String_Decoded = JsonWebToken.Decode(JWT_Encoded_String, "", false);
-					JObject JWT_Decoded = JObject.Parse(JWT_String_Decoded);
-					long _ticks = long.Parse(JWT_Decoded["exp"].ToString());
-					DateTime _exp = new DateTime(_ticks);
-					if (_exp >= DateTime.Now)
-					{
-						var alert = new UIAlertView("Error", "This token is expired", null, "Ok", null);
-						alert.Show();
-						return;
-					}
-					tbCode.Text = JWT_Decoded["aud"].ToString();
-					user.SetString(JWT_Decoded["iss"].ToString(), "PORTAL");
-					generateCodeAndNavigate(tbCode.Text);
-					scanner = null;
-					result = null;
-					GC.Collect();
-				}
-			}
-			catch (Exception ex)
-			{
-				var alert = new UIAlertView("Error", ex.Message, null, "Ok", null);
-				alert.Show();
-			}
-		}
+
     }
 }
